8c14409c9195d4388b2da9f40dd2c4a13ed8b33d
Trystan Eveno <trystan.eveno@lephare.com>
2018-08-27 14:47:40 +0200

    edit color lvl of redshift

--- .config/polybar/scripts/redshift.sh
+++ .config/polybar/scripts/redshift.sh
@@ -4,12 +4,12 @@ if [ "$(pgrep -x redshift)" ]; then
     temp=$(redshift -p 2> /dev/null | grep temp | cut -d ":" -f 2 | tr -dc "[:digit:]")
 
     if [ -z "$temp" ]; then
-        echo "%{F#65737E} #"
+        echo "%{F#0066ff}  "
     elif [ "$temp" -ge 5000 ]; then
-        echo "%{F#8FA1B3} #"
+        echo "%{F#66ff66}  "
     elif [ "$temp" -ge 4000 ]; then
-        echo "%{F#EBCB8B} #"
+        echo "%{F#ff9933}  "
     else
-        echo "%{F#D08770} #"
+        echo "%{F#ff3300}  "
     fi
 fi
