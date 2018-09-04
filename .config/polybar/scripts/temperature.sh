c981fda05c4d1da1952c296493e266235d16376c
Trystan Eveno <trystan.eveno@lephare.com>
2018-09-05 00:03:51 +0200

    change icons temperature scripts

--- .config/polybar/scripts/temperature.sh
+++ .config/polybar/scripts/temperature.sh
@@ -4,15 +4,15 @@ if [ "$(pgrep -x redshift)" ]; then
     temp=$(sensors | grep Core | awk '{print substr($3, 2, length($3)-6)}' | head -n 1)
 
     if [ "$temp" -lt 25 ]; then
-      echo "%{F#0066ff}"
+      echo "%{F#0066ff}"
     elif [ "$temp" -ge 25 ] && [ "$temp" -lt 50 ]; then
-      echo "%{F#00cc00}"
+      echo "%{F#00cc00}"
     elif [ "$temp" -ge 50 ] && [ "$temp" -lt 75 ]; then
-      echo "%{F#ffff00}"
+      echo "%{F#ffff00}"
     elif [ "$temp" -ge 75 ] && [ "$temp" -lt 90 ]; then
       echo "%{F#ff9933}"
     elif [ "$temp" -ge 90 ]; then
-      echo "%{F#ff3300}"
+      echo "%{F#ff3300}"
     else
       echo ""
     fi
