a535856be1b38005c4c444dbf0704738249c0a7f
Trystan Eveno <trystan.eveno@gmail.com>
2021-09-17 17:27:52 +0200

    Come back to life

--- .config/polybar/scripts/light.sh
+++ .config/polybar/scripts/light.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
 
-temp=$(xbacklight)
-lt_10=$(echo "scale=2;$temp < 10.00" | bc)
-
-if [ "$lt_10" -eq 1 ]; then
-  temp=$(xbacklight | colrm 2)
-else
-  temp=$(xbacklight | colrm 3)
-fi
+temp=$(xbacklight -get)
 
 if [ "$temp" -lt 10 ]; then
-  echo "%{F#444444}"
+  echo "$temp  %{F#444444}"
 elif [ "$temp" -ge 10 ] && [ "$temp" -lt 20 ]; then
-  echo "%{F#555555}"
+  echo "$temp  %{F#555555}"
 elif [ "$temp" -ge 20 ] && [ "$temp" -lt 30 ]; then
-  echo "%{F#dfdfdf}"
+  echo "$temp  %{F#dfdfdf}"
 elif [ "$temp" -ge 30 ] && [ "$temp" -lt 40 ]; then
-  echo "%{F#c0ffff}"
+  echo "$temp  %{F#c0ffff}"
 elif [ "$temp" -ge 40 ] && [ "$temp" -lt 50 ]; then
-  echo "%{F#2ecfff}"
+  echo "$temp  %{F#2ecfff}"
 elif [ "$temp" -ge 50 ] && [ "$temp" -lt 60 ]; then
-  echo "%{F#0066ff}"
+  echo "$temp  %{F#0066ff}"
 elif [ "$temp" -ge 60 ] && [ "$temp" -lt 70 ]; then
-  echo "%{F#00cc00}"
+  echo "$temp  %{F#00cc00}"
 elif [ "$temp" -ge 70 ] && [ "$temp" -lt 80 ]; then
-  echo "%{F#ffff00}"
+  echo "$temp  %{F#ffff00}"
 elif [ "$temp" -ge 80 ] && [ "$temp" -lt 90 ]; then
-  echo "%{F#ff9933}"
+  echo "$temp  %{F#ff9933}"
 elif [ "$temp" -ge 90 ]; then
-  echo "%{F#ff3300}"
+  echo "$temp  %{F#ff3300}"
 else
-  echo ""
+  echo "$temp  "
 fi
