0dfa692a910842cd6b96d2fd292a1bf5854df4cf
Trystan Eveno <trystan.eveno@gmail.com>
2022-02-02 16:29:40 +0100

    Hide notifs when screen locked

--- .config/i3/beautify_lock.sh
+++ .config/i3/beautify_lock.sh
@@ -13,7 +13,10 @@ done
 TMPBG=/tmp/screen.png
 scrot $TMPBG && convert $TMPBG -scale 5% -scale 2000% -draw "fill black fill-opacity 0.4 $rectangles" $TMPBG
 
+pkill -u "$USER" -USR1 dunst
+
 i3lock \
+  -n \
   -i $TMPBG \
   --time-pos="x+110:h-120" --date-pos="tx+24:ty+25" \
   --clock --indicator --date-str "Type password to unlock..." \
@@ -23,5 +26,7 @@ i3lock \
   --ringver-color=ffffffff --ringwrong-color=ffffffff --ind-pos="x+290:h-120" \
   --radius=20 --ring-width=3 --verif-text="" --wrong-text="" --noinput-text="" \
   --time-color=ffffffff --date-color=ffffffff
+
+pkill -u "$USER" -USR2 dunst
 
 rm $TMPBG
