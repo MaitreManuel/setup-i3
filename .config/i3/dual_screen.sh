6d9f5402f9f265c4ff57f469a929805f304de593
Trystan Eveno <trystan.eveno@gmail.com>
2022-09-30 15:51:13 +0200

    Update dual_screen.sh

--- .config/i3/dual_screen.sh
+++ .config/i3/dual_screen.sh
@@ -6,16 +6,16 @@ main () {
   dp3_plug=$(xrandr --current | grep '^DP-3 connected')
 
   if [ "$dp1_plug" != "" ] && [ "$dp3_plug" != "" ]; then
-    echo "DP1 & DP3 plug on"
+    echo "DP-1 & DP-3 plug on"
     xrandr --output DP-1 --auto --left-of DP-3 --output DP-3 --auto --above eDP-1 --output eDP-1 --auto
   elif [ "$dp1_plug" = "" ] && [ "$dp3_plug" = "" ]; then
-    echo "DP1 and DP3 plug off"
+    echo "DP-1 and DP-3 plug off"
     xrandr --auto
   elif [ "$dp1_plug" != "" ]  && [ "$dp3_plug" = "" ]; then
-    echo "DP1 plug on"
+    echo "DP-1 plug on"
     xrandr --output DP-1 --auto --above eDP-1 --output eDP-1 --auto
   elif [ "$dp1_plug" = "" ]  && [ "$dp3_plug" != "" ]; then
-    echo "DP3 plug on"
+    echo "DP-3 plug on"
     xrandr --output DP-3 --auto --above eDP-1 --output eDP-1 --auto
   else
     echo "something went wrong"
